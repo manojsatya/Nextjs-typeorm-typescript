7068b8bb2020290021c42aaa993d2a3d52563513
manojsatya <manoj.pesit@gmail.com>
2021-01-23 22:31:44 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+// import { User } from "./entity/User";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(5000, async () => {
+  console.log("Server running at http://localhost:5000");
+
+  try {
+    await createConnection();
+    console.log("Database connected");
+  } catch (err) {
+    console.log(err);
+  }
+});
